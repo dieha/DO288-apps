a61944a91ba1e016e45f83782dc2f549dc1a7093
dieha <dieha8066@gmail.com>
2021-08-26 18:00:43 +0800

    Update app.js

--- build-app/app.js
+++ build-app/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
+  console.log('Simple app for the Building Applications Lab!');
   res.send('Simple app for the Building Applications Lab!\n');
 });
 
